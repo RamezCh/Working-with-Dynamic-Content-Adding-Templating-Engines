14a0df398cc9b7c74bf0815955f2f877d0c1a8a4
RamezCh <ramiz_chreide@outlook.com>
2024-09-14 00:03:19 +0300

    Sharing Data across Requests & Users

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,9 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
-
+// Shared accross all requests to this file
+// If you open diff browser, it will show the same products
+const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -13,8 +15,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
