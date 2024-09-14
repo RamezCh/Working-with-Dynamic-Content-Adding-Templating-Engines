3445d959497cbc6c51492540e5f72a001f9f402d
RamezCh <ramiz_chreide@outlook.com>
2024-09-14 08:20:52 +0300

    Convering HTML files to PUG

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/add-product => POST
