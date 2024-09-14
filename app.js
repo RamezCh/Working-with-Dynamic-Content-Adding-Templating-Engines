43b4ff352c1557b357d6c6e1314ea1bdfe6b5fdd
RamezCh <ramiz_chreide@outlook.com>
2024-09-14 14:50:32 +0300

    Working with Handlebars

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+// We used engine method because handlebars isn't built-in express
+app.engine('hbs', expressHbs()); // extension depends on what we call it here
 // Global configuration for the view engine
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs'); // was pug instead of handlebars
 // If your views are in a folder called views, you don't need to set the path
 // app.set('views', 'views');
 
@@ -19,6 +23,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
+  // passing data doesn't change with engine, just way of usage in template differs
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,12 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   const products = adminData.products;
   // When using templating engines, you can pass an object with data to the view
-  res.render('shop', { products, pageTitle: 'Shop', path: '/shop' });
+  res.render('shop', {
+    products,
+    pageTitle: 'Shop',
+    path: '/shop',
+    hasProducts: products.length > 0,
+  });
 });
 // Path doesn't have to be real path, anything we want
 module.exports = router;
