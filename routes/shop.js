57ad1ffe87be8581710061392dadfc8cc4509f37
RamezCh <ramiz_chreide@outlook.com>
2024-09-14 08:33:47 +0300

    Finishing the Pug template

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   const products = adminData.products;
   // When using templating engines, you can pass an object with data to the view
-  res.render('shop', { products, pageTitle: 'Shop' });
+  res.render('shop', { products, pageTitle: 'Shop', path: '/shop' });
 });
-
+// Path doesn't have to be real path, anything we want
 module.exports = router;
