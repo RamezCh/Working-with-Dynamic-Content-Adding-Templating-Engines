fccda93517a1cf3b74a56e4304a3290c6dbc1bf0
RamezCh <ramiz_chreide@outlook.com>
2024-09-15 14:40:41 +0300

    Working with EJS

---
+++ app-v1.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+// We used engine method because handlebars isn't built-in express
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  })
+); // extension depends on what we call it here
+// Global configuration for the view engine
+app.set('view engine', 'hbs'); // was pug instead of handlebars
+// If your views are in a folder called views, you don't need to set the path
+// app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  // passing data doesn't change with engine, just way of usage in template differs
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
--- app.js
+++ app.js
@@ -2,23 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// We used engine method because handlebars isn't built-in express
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-); // extension depends on what we call it here
-// Global configuration for the view engine
-app.set('view engine', 'hbs'); // was pug instead of handlebars
-// If your views are in a folder called views, you don't need to set the path
-// app.set('views', 'views');
+app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -30,8 +17,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // passing data doesn't change with engine, just way of usage in template differs
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 });
 
 app.listen(3000);
